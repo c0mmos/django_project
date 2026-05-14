dc7b8ad4a94cf28d01c372ce9ff98732b4501a7f
c0mm0s <multicolorr847@gmail.com>
2026-05-14 10:46:48 +0330

    dynamic url

--- base_site/urls.py
+++ base_site/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
 from base_site.views import *
 
+app_name = 'website'
+
 urlpatterns = [
-    path('', index_home),
-    path('contact', index_contact),
-    path('about', index_about),
-    path('elements', index_elements)
+    path('', index_home, name='index'),
+    path('contact', index_contact, name='contact'),
+    path('about', index_about, name='about'),
+    path('elements', index_elements, name='elements')
 ]
