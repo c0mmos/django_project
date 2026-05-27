22e741098d20263a9f562318cbe45916f60a40f7
c0mm0s <multicolorr847@gmail.com>
2026-05-27 12:41:00 +0330

    created admin panel
    now you can control from admin panel

--- base_site/admin.py
+++ base_site/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from base_site.models import contact
 # Register your models here.
+
+
+class ContactAdmin(admin.ModelAdmin):
+    date_hierarchy = "created_date"
+    list_display = ['name', 'email', 'created_date']
+    list_filter = ('email',)
+    search_fields = ('name', 'subject', 'message')
+    ordering = ["-created_date"]
+    
+admin.site.register(contact, ContactAdmin)
--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from blog.models import Post
 # Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    date_hierarchy = "created_date"
+    list_display = ('title','counted_views','status','published_date','created_date')
+    list_filter = ('status',)
+    search_fields = ('title', 'content')
+    ordering = ['-created_date']
+
+admin.site.register(Post, PostAdmin)
