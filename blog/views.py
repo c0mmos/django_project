27691bfda37dcf25a90c038c5a389db4275050b6
c0mm0s <multicolorr847@gmail.com>
2026-05-30 18:26:21 +0330

    fixed blog
    i totally fixed the blog app now you can add a post and it appears on the site

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from blog.models import Post
 
 # Create your views here.
 def index_blog(requests):
-    return render(requests, "blog/blog-home.html")
+    context = {'posts' : Post.objects.filter(status=1)}
+    return render(requests, "blog/blog-home.html", context)
 
 def single_blog(requests):
     return render(requests, "blog/blog-single.html")
