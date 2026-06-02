93e51e50850fa905a445bc6141d5c22ccb0a7fe1
c0mm0s <multicolorr847@gmail.com>
2026-06-02 19:46:27 +0330

    added pre/next
    now you can see the previous and next post on blog details

--- blog/models.py
+++ blog/models.py
@@ -3,10 +3,17 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
 class Post(models.Model):
     title = models.CharField(max_length=255)
     author = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     image = models.ImageField(upload_to='blog', default='/blog/default.jpg')
+    category = models.ManyToManyField(Category)
     content = models.CharField(max_length=255)
     counted_views = models.IntegerField(default=0)
     status = models.BooleanField(default=False)
--- blog/views.py
+++ blog/views.py
@@ -11,9 +11,24 @@ def index_blog(requests):
 def single_blog(requests, pid):
     posts = Post.objects.filter(status=1).exclude(published_date__gt=datetime.now())
     post = get_object_or_404(posts, pk=pid)
-    context = {'post': post}
 
     Post.objects.filter(id=pid).update(counted_views=F('counted_views') + 1)
+
+    previous_post = (
+        Post.objects
+        .filter(published_date__lt=post.published_date, status=1)
+        .order_by('-published_date')
+        .first()
+    )
+
+    next_post = (
+        Post.objects
+        .filter(published_date__gt=post.published_date, status=1)
+        .order_by('published_date')
+        .first()
+    )
+
+    context = {'post': post, 'previous_post': previous_post, 'next_post': next_post}
 
     post.refresh_from_db()
     return render(requests, "blog/blog-single.html", context=context)
