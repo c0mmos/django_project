09c51cf7d887b5504837360cd4bfc99e357c4f41
c0mm0s <multicolorr847@gmail.com>
2026-06-03 10:03:43 +0330

    fixed image issues

--- blog/views.py
+++ blog/views.py
@@ -2,14 +2,15 @@ from django.shortcuts import render, get_object_or_404
 from blog.models import Post
 from datetime import datetime
 from django.db.models import F
+from django.utils import timezone
 
 # Create your views here.
 def index_blog(requests):
-    context = {'posts' : Post.objects.filter(status=1).exclude(published_date__gt=datetime.now())}
+    context = {'posts': Post.objects.filter(status=1, published_date__lte=timezone.now()).order_by('-published_date')}
     return render(requests, "blog/blog-home.html", context)
 
 def single_blog(requests, pid):
-    posts = Post.objects.filter(status=1).exclude(published_date__gt=datetime.now())
+    posts = Post.objects.filter(status=1, published_date__lte=timezone.now())
     post = get_object_or_404(posts, pk=pid)
 
     Post.objects.filter(id=pid).update(counted_views=F('counted_views') + 1)
